a1bfeee877fdcfdfe70bdc8ffc80639ad3ab023c
Maksim <pumpbro@gmail.com>
2024-03-05 20:44:36 +0300

    fix spelling

--- tests/test_find_duplicates.py
+++ tests/test_find_duplicates.py
@@ -2,28 +2,28 @@ from subprocess import run, PIPE
 import os
 import pytest
 
-B_PATH_FILE = './bin/find_duplecates'
+B_PATH_FILE = './bin/find_duplicates'
 
-def test_bin_folder_contains_find_duplecates():
+def test_bin_folder_contains_find_duplicates():
     assert os.path.isfile(B_PATH_FILE)
 
-def test_find_duplecates_not_correct_input():
-    result = run([B_PATH_FILE], input="../tests/find_duplecates/file3.txt ../tests/find_duplecates/file1.txt", stderr=PIPE, stdout=PIPE)
+def test_find_duplicates_not_correct_input():
+    result = run([B_PATH_FILE], input="../tests/find_duplicates/file3.txt ../tests/find_duplicates/file1.txt", stderr=PIPE, stdout=PIPE)
     assert result.returncode != 0
     assert "Puck you, Verter!" in result.stderr
 
-def test_find_duplecates_correct_input():
-    file_1 = "../tests/find_duplecates/file1.txt"
-    file_2 = "../tests/find_duplecates/file2.txt"
+def test_find_duplicates_correct_input():
+    file_1 = "../tests/find_duplicates/file1.txt"
+    file_2 = "../tests/find_duplicates/file2.txt"
     result = run([B_PATH_FILE], input=f"{file_1} {file_2}", stdout=PIPE)
     assert os.path.isfile(file_1)
     assert os.path.isfile(file_2) == False
     assert result.stdout == ""
     assert result.returncode == 0
 
-def test_find_duplecates_empty_files():
-    file_1 = "../tests/find_duplecates/empty1.txt"
-    file_2 = "../tests/find_duplecates/empty2.txt"
+def test_find_duplicates_empty_files():
+    file_1 = "../tests/find_duplicates/empty1.txt"
+    file_2 = "../tests/find_duplicates/empty2.txt"
     result = run([B_PATH_FILE], input=f"{file_1} {file_2}", stdout=PIPE)
     assert os.path.isfile(file_1)
     assert os.path.isfile(file_2) == False
